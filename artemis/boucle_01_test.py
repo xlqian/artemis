ce8ccf516e314d900d2b3d467b076ec05321230d
Patrick Degeilh <patrick.degeilh@canaltp.fr>
2014-07-17 11:03:26 +0200

    update test name boucle-01

--- artemis/boucle_01_test.py
+++ artemis/boucle_01_test.py
@@ -3,19 +3,19 @@ from artemis.test_mechanism import ArtemisTestFixture, dataset
 #TODO: rename the test
 
 @dataset(["boucle-01"])
-class TestBoucle(ArtemisTestFixture):
+class TestBoucle01(ArtemisTestFixture):
     """
     TODO: put there comments about the dataset
     """
 
-    def test_boucle_01(self):
+    def test_boucle_01_01(self):
         self.journey(_from="stop_area:BC1:SA:1",
                      to="stop_area:BC1:SA:6", datetime="20041213T0730")
 
-    def test_boucle_02(self):
+    def test_boucle_01_02(self):
         self.journey(_from="stop_area:BC1:SA:3",
                      to="stop_area:BC1:SA:7", datetime="20041213T0730")
 
-    def test_boucle_03(self):
+    def test_boucle_01_03(self):
         self.journey(_from="stop_area:BC1:SA:8",
                      to="stop_area:BC1:SA:5", datetime="20041213T0730")
