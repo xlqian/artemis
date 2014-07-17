71c36073e9131ce62572904376d783ef23f14f85
Patrick Degeilh <patrick.degeilh@canaltp.fr>
2014-07-17 16:50:49 +0200

    port jmeter test-01 tests to artemis

---
+++ artemis/test_01_test.py
@@ -0,0 +1,16 @@
+from artemis.test_mechanism import ArtemisTestFixture, dataset
+
+
+@dataset(["test-01"])
+class TestTest01(ArtemisTestFixture):
+    """
+    TODO: put there comments about the dataset
+    """
+    def test_test_01_01(self):
+        self.journey(_from="stop_area:TS1:SA:2",
+                     to="stop_area:TS1:SA:6", datetime="20041214T0700")
+
+    def test_test_01_02(self):
+        self.journey(_from="stop_area:TS1:SA:10",
+                     to="stop_area:TS1:SA:12", datetime="20041214T0700")
+
