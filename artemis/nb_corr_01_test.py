94c59315838fadd41fcc4385400c0e699ed65eac
Patrick Degeilh <patrick.degeilh@canaltp.fr>
2014-07-17 14:41:53 +0200

    port jmeter nb-corr-01 tests to artemis

---
+++ artemis/nb_corr_01_test.py
@@ -0,0 +1,11 @@
+from artemis.test_mechanism import ArtemisTestFixture, dataset
+
+
+@dataset(["nb-corr-01"])
+class TestNbCorr01(ArtemisTestFixture):
+    """
+    TODO: put there comments about the dataset
+    """
+    def test_nb_corr_01_01(self):
+        self.journey(_from="stop_area:NC1:SA:1",
+                     to="stop_area:NC1:SA:4", datetime="20041213T0700")
