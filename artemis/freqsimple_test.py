b96abc5aeb97934cf537a0f2ddda53be97ea1155
Patrick Degeilh <patrick.degeilh@canaltp.fr>
2014-07-17 14:29:15 +0200

    port jmeter freqsimple tests to artemis

---
+++ artemis/freqsimple_test.py
@@ -0,0 +1,27 @@
+from artemis.test_mechanism import ArtemisTestFixture, dataset
+
+
+@dataset(["freqsimple"])
+class TestFreqSimple(ArtemisTestFixture):
+    """
+    TODO: put there comments about the dataset
+    """
+    def test_freqsimple_01(self):
+        self.journey(_from="stop_area:FQS:SA:GRA",
+                     to="stop_area:FQS:SA:MBC", datetime="20090922T0700")
+
+    def test_freqsimple_02(self):
+        self.journey(_from="stop_area:FQS:SA:GRA",
+                     to="stop_area:FQS:SA:MBC", datetime="20070417T0500")
+
+    def test_freqsimple_03(self):
+        self.journey(_from="stop_area:FQS:SA:MBC",
+                     to="stop_area:FQS:SA:GRA", datetime="20070417T0100")
+
+    def test_freqsimple_04(self):
+        self.journey(_from="stop_area:FQS:SA:MBC",
+                     to="stop_area:FQS:SA:GRA", datetime="20070417T0520", datetime_represents="arrival")
+
+    def test_freqsimple_05(self):
+        self.journey(_from="stop_area:FQS:SA:MBC",
+                     to="stop_area:FQS:SA:GRA", datetime="20070417T0550", datetime_represents="arrival")
