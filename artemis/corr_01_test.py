3a8a94877a64948047c477eeb8d935bdd11dd328
patochectp <patrick.degeilh@canaltp.fr>
2014-08-29 11:37:48 +0200

    Update corr_01_test.py

--- artemis/corr_01_test.py
+++ artemis/corr_01_test.py
@@ -4,9 +4,13 @@ from artemis.test_mechanism import ArtemisTestFixture, dataset
 @dataset(["corr-01"])
 class TestCorr01(ArtemisTestFixture):
     """
-    TODO: put there comments about the dataset
+    Le test test_corr_01_01 ne renvoie pas d'itinéraire car le nombre de correspondance est > 10
     """
 
     def test_corr_01_01(self):
+        self.journey(_from="stop_area:CR1:SA:1",
+                     to="stop_area:CR1:SA:13", datetime="20041210T0700")
+
+    def test_corr_01_02(self):
         self.journey(_from="stop_area:CR1:SA:1",
                      to="stop_area:CR1:SA:13", datetime="20041210T0700", max_nb_transfers=12)
