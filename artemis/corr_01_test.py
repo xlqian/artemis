d82f17b5df08e93232315b7c5e25f46f5b296f16
patochectp <patrick.degeilh@canaltp.fr>
2014-08-29 11:48:39 +0200

    Add test on max_nb_transfers

--- artemis/corr_01_test.py
+++ artemis/corr_01_test.py
@@ -4,13 +4,15 @@ from artemis.test_mechanism import ArtemisTestFixture, dataset
 @dataset(["corr-01"])
 class TestCorr01(ArtemisTestFixture):
     """
-    Le test test_corr_01_01 ne renvoie pas d'itinéraire car le nombre de correspondance est > 10
     """
 
     def test_corr_01_01(self):
+        """
+        no solution found for this journey because number of transfers > 10
+        """
         self.journey(_from="stop_area:CR1:SA:1",
                      to="stop_area:CR1:SA:13", datetime="20041210T0700")
 
     def test_corr_01_02(self):
         self.journey(_from="stop_area:CR1:SA:1",
-                     to="stop_area:CR1:SA:13", datetime="20041210T0700", max_nb_transfers=12)
+                     to="stop_area:CR1:SA:13", datetime="20041210T0700", max_nb_transfers=11)
