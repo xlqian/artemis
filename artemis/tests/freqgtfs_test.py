99e8acc5bc8ec4a50fb6c54a27d5980d6b0a4a96
patochectp <patrick.degeilh@canaltp.fr>
2014-09-03 17:30:28 +0200

    add freqgtfs tests

--- artemis/tests/freqgtfs_test.py
+++ artemis/tests/freqgtfs_test.py
@@ -11,3 +11,23 @@ class TestFreqGtfs(ArtemisTestFixture):
         self.journey(_from="stop_area:FQG:SA:35",
                      to="stop_area:FQG:SA:1",
                      datetime="20070417T054000")
+
+    def test_freqgtfs_02(self):
+        self.journey(_from="stop_area:FQG:SA:35",
+                     to="stop_area:FQG:SA:1",
+                     datetime="20070417T050000")
+
+    def test_freqgtfs_03(self):
+        self.journey(_from="stop_area:FQG:SA:35",
+                     to="stop_area:FQG:SA:1",
+                     datetime="20070417T010000")
+
+    def test_freqgtfs_04(self):
+        self.journey(_from="stop_area:FQG:SA:35",
+                     to="stop_area:FQG:SA:1",
+                     datetime="20070417T052000", datetime_represents="arrival")
+
+    def test_freqgtfs_05(self):
+        self.journey(_from="stop_area:FQG:SA:1",
+                     to="stop_area:FQG:SA:35",
+                     datetime="20070417T055000", datetime_represents="arrival")
