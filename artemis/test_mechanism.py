195b236c936527e8a051908b0a28d5eb3de10c8a
patochectp <patrick.degeilh@canaltp.fr>
2014-10-22 15:32:17 +0200

    remove try/catch

--- artemis/test_mechanism.py
+++ artemis/test_mechanism.py
@@ -91,18 +91,13 @@ class ArtemisTestFixture:
         if new dataset exist we must unzip in data directory to consider in "read_data"
         """
         for data_set in cls.data_sets:
-            logging.getLogger(__name__).info("updating data for {}".format(
-                data_set))
-            try:
-                zip_filename = zip_path(data_set)
-                if os.path.exists(zip_filename):
-                    zip_file = zipfile.ZipFile(zip_filename)
-                    zip_file.extractall(path=dir_path(data_set))
-                    os.remove(zip_filename)
-            except:
-                logging.getLogger(__name__).exception("can't updating data")
-                assert "problem while updating data"
-
+            logging.getLogger(__name__).info("updating data for {}".format(data_set))
+            zip_filename = zip_path(data_set)
+            if not os.path.exists(zip_filename):
+                continue
+            zip_file = zipfile.ZipFile(zip_filename)
+            zip_file.extractall(path=dir_path(data_set))
+            os.remove(zip_filename)
 
     @classmethod
     def teardown_class(cls):
