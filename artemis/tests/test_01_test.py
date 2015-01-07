00830f35ab19003b66e9f97d3d11eed8d4690141
patochectp <patrick.degeilh@canaltp.fr>
2015-01-07 17:33:48 +0100

    manage test-01 fail

--- artemis/tests/test_01_test.py
+++ artemis/tests/test_01_test.py
@@ -1,11 +1,13 @@
 from artemis.test_mechanism import ArtemisTestFixture, dataset, DataSet
-
+import pytest
+xfail = pytest.mark.xfail
 
 @dataset([DataSet("test-01")])
 class TestTest01(ArtemisTestFixture):
     """
     TODO: put there comments about the dataset
     """
+    @xfail(reason="http://jira.canaltp.fr/browse/NAVITIAII-1126", raises=AssertionError)
     def test_test_01_01(self):
         self.journey(_from="stop_area:TS1:SA:2",
                      to="stop_area:TS1:SA:6", datetime="20041214T0700")
