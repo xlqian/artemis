a16f6ebd98de4602c1eb7dcd6ffe55bb3fa3eccc
patochectp <patrick.degeilh@canaltp.fr>
2015-02-11 17:02:07 +0100

    fix indentation

--- artemis/tests/itl_test.py
+++ artemis/tests/itl_test.py
@@ -8,13 +8,13 @@ class TestItl(ArtemisTestFixture):
     test local traffic policy constraint
     """
 
-	@xfail(reason="http://jira.canaltp.fr/browse/NAVITIAII-1463", raises=AssertionError)
+    @xfail(reason="http://jira.canaltp.fr/browse/NAVITIAII-1463", raises=AssertionError)
     def test_itl_01(self):
         self.journey(_from="stop_area:ITL:SA:1",
                      to="stop_area:ITL:SA:7",
                      datetime="20041213T070000")
 
-	@xfail(reason="http://jira.canaltp.fr/browse/NAVITIAII-1584", raises=AssertionError)
+    @xfail(reason="http://jira.canaltp.fr/browse/NAVITIAII-1584", raises=AssertionError)
     def test_itl_02(self):
         self.journey(_from="stop_area:ITL:SA:1",
                      to="stop_area:ITL:SA:7",
