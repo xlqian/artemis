cabe51e7ee35a343e4337b5e7882db3f05d5b502
Vincent Lara <lara.vincent@gmail.com>
2015-03-06 14:40:59 +0100

    Remove xfail from fix_prolong_mano_test

--- artemis/tests/prolong_mano_test.py
+++ artemis/tests/prolong_mano_test.py
@@ -7,12 +7,10 @@ class TestProlongMano(ArtemisTestFixture):
     """
     TODO: put there comments about the dataset
     """
-    @xfail(reason="http://jira.canaltp.fr/browse/NAVITIAII-1123", raises=AssertionError)
     def test_prolong_mano_01(self):
         self.journey(_from="stop_area:PRM:SA:1",
                      to="stop_area:PRM:SA:5", datetime="20041213T0700")
 
-    @xfail(reason="http://jira.canaltp.fr/browse/NAVITIAII-1588", raises=AssertionError)
     def test_prolong_mano_02(self):
         self.journey(_from="stop_area:PRM:SA:1",
                      to="stop_area:PRM:SA:9", datetime="20041213T0700")
