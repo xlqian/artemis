588ddb2b7c257b2f87e4e7a7ebe675cffe3e22f9
patochectp <patrick.degeilh@canaltp.fr>
2015-06-08 13:04:19 +0200

    delete xfail tests

--- artemis/tests/freqparis_test.py
+++ artemis/tests/freqparis_test.py
@@ -1,13 +1,11 @@
 from artemis.test_mechanism import ArtemisTestFixture, dataset, DataSet
 import pytest
-xfail = pytest.mark.xfail
 
 @dataset([DataSet("freqparis")])
 class TestFreqParis(ArtemisTestFixture):
     """
     TODO: put there comments about the dataset
     """
-    @xfail(reason="http://jira.canaltp.fr/browse/NAVITIAII-1459", raises=AssertionError)
     def test_freqparis_01(self):
         self.journey(_from="stop_area:FQP:SA:defen",
                      to="stop_area:FQP:SA:grest", datetime="20090922T0700")
@@ -20,12 +18,10 @@ class TestFreqParis(ArtemisTestFixture):
         self.journey(_from="stop_area:FQP:SA:defen",
                      to="stop_area:FQP:SA:grest", datetime="20090922T2200")
 
-    @xfail(reason="http://jira.canaltp.fr/browse/NAVITIAII-1579", raises=AssertionError)
     def test_freqparis_04(self):
         self.journey(_from="stop_area:FQP:SA:defen",
                      to="stop_area:FQP:SA:grest", datetime="20090922T0008", datetime_represents="arrival")
 
-    @xfail(reason="http://jira.canaltp.fr/browse/NAVITIAII-1580", raises=AssertionError)
     def test_freqparis_05(self):
         self.journey(_from="stop_area:FQP:SA:defen",
                      to="stop_area:FQP:SA:grest", datetime="20090922T0020", datetime_represents="arrival")
