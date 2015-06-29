658e506f4685bacdb17d1c6fc7aa3622d304bffc
Pierre-Etienne Bougue <pierre-etienne.bougue@canaltp.fr>
2015-06-29 17:34:10 +0200

    now that crow fly is computed, test with duration max null

--- artemis/tests/corr_01_test.py
+++ artemis/tests/corr_01_test.py
@@ -11,8 +11,9 @@ class TestCorr01(ArtemisTestFixture):
         no solution found for this journey because number of transfers > 10
         """
         self.journey(_from="stop_area:CR1:SA:1",
-                     to="stop_area:CR1:SA:13", datetime="20041210T0700")
+                     to="stop_area:CR1:SA:13", datetime="20041210T0700", max_duration_to_pt=0)
 
     def test_corr_01_02(self):
         self.journey(_from="stop_area:CR1:SA:1",
-                     to="stop_area:CR1:SA:13", datetime="20041210T0700", max_nb_transfers=11)
+                     to="stop_area:CR1:SA:13", datetime="20041210T0700", max_nb_transfers=11,
+                     max_duration_to_pt=0)
