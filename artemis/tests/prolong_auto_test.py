804f9ada5ad3c6a199b55faaaa8f94d2168beba7
Pierre-Etienne Bougue <pierre-etienne.bougue@canaltp.fr>
2015-06-30 16:36:21 +0200

    test update for crow fly feature

--- artemis/tests/prolong_auto_test.py
+++ artemis/tests/prolong_auto_test.py
@@ -13,7 +13,7 @@ class TestProlongAuto(ArtemisTestFixture):
 
     def test_prolong_auto_02(self):
         self.journey(_from="stop_area:PRA:SA:1",
-                     to="stop_area:PRA:SA:9", datetime="20041213T0700")
+                     to="stop_area:PRA:SA:9", datetime="20041213T0700", max_duration_to_pt=0)
 
     def test_prolong_auto_03(self):
         self.journey(_from="stop_area:PRA:SA:1",
