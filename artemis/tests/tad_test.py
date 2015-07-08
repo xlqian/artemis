a1d6fbdc818197ba4953e15f62f7a4020c8b641b
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2015-07-08 14:14:46 +0200

    artemis: comments added
    coordinates invalid

--- artemis/tests/tad_test.py
+++ artemis/tests/tad_test.py
@@ -4,47 +4,54 @@ from artemis.test_mechanism import ArtemisTestFixture, dataset, DataSet
 @dataset([DataSet("tad")])
 class TestTad(ArtemisTestFixture):
     """
-    On demand transport tests
+    On demand transport tests : City to City
     """
-    # Tad commune a commune
     def test_city_2_city(self):
         self.journey(_from="admin:41246",
                      to="admin:41266",
                      datetime="20150302T102000")
 
+    # to is a coord in admin : admin:41266
     def test_city_2_coord(self):
         self.journey(_from="admin:41246",
                      to="1.2391;47.4599",
                      datetime="20150302T102000")
 
+    # _from is a coord in admin : admin:41246
     def test_coord_2_city(self):
-        self.journey(_from="1.3347;475096",
+        self.journey(_from="1.3379;47.5096",
                      to="admin:41266",
                      datetime="20150302T102000")
 
+    # _from is a coord in admin : admin:41246
+    # to is a coord in admin : admin:41266
     def test_coord_2_coord(self):
-        self.journey(_from="1.3347;475096",
+        self.journey(_from="1.3379;47.5096",
                      to="1.2391;47.4599",
                      datetime="20150302T102000")
 
-    # Tad commune a arret
+    """
+    On demand transport tests : City to Station
+    """
     def test_stop_point_2_city(self):
         self.journey(_from="stop_point:CAA:SP:blr2",
                      to="admin:41212",
                      datetime="20150310T102000")
 
     def test_stop_area_2_city(self):
-        self.journey(_from="stop_area:T18:SA:blr1",
+        self.journey(_from="stop_area:CAA:SA:blr2",
                      to="admin:41212",
                      datetime="20150310T102000")
 
+    # to is a coord in admin : admin:41212
     def test_stop_point_2_coord(self):
         self.journey(_from="stop_point:CAA:SP:blr2",
                      to="1.3573;47.5564",
                      datetime="20150310T102000")
 
+    # to is a coord in admin : admin:41212
     def test_stop_area_2_coord(self):
-        self.journey(_from="stop_area:T18:SA:blr1",
+        self.journey(_from="stop_area:CAA:SA:blr2",
                      to="1.3573;47.5564",
                      datetime="20150310T102000")
 
@@ -55,20 +62,25 @@ class TestTad(ArtemisTestFixture):
 
     def test_city_2_stop_area(self):
         self.journey(_from="admin:41212",
-                     to="stop_area:T18:SA:blr1",
+                     to="stop_area:CAA:SA:blr2",
                      datetime="20150310T102000")
 
+    # _from is a coord in admin : admin:41212
     def test_coord_2_stop_point(self):
         self.journey(_from="1.3573;47.5564",
                      to="stop_point:CAA:SP:blr2",
                      datetime="20150310T102000")
 
+    # _from is a coord in admin : admin:41212
     def test_coord_2_stop_area(self):
         self.journey(_from="1.3573;47.5564",
                      to="stop_area:T18:SA:blr1",
                      datetime="20150310T102000")
 
-    # Tad commune a arret (depart apres)
+    """
+    On demand transport tests : City to Station
+        Departure from station with hour fixed
+    """
     def test_stop_point_2_city_dep_after(self):
         self.journey(_from="stop_point:CA2:SP:blr1",
                      to="admin:41295",
@@ -79,6 +91,7 @@ class TestTad(ArtemisTestFixture):
                      to="admin:41295",
                      datetime="20150313T102000")
 
+    # to is a coord in admin : admin:41295
     def test_stop_point_2_coord_dep_after(self):
         self.journey(_from="stop_point:CA2:SP:blr1",
                      to="1.3828;47.5831",
@@ -99,6 +112,7 @@ class TestTad(ArtemisTestFixture):
                      to="stop_area:CA2:SA:blr1",
                      datetime="20150313T102000")
 
+    # _from is a coord in admin : admin:41295
     def test_coord_2_stop_point_dep_after(self):
         self.journey(_from="1.3828;47.5831",
                      to="stop_point:CA2:SP:blr1",
@@ -109,7 +123,10 @@ class TestTad(ArtemisTestFixture):
                      to="stop_area:CA2:SA:blr1",
                      datetime="20150313T102000")
 
-    # Tad commune a arret (arrivee avant)
+    """
+    On demand transport tests : City to Station
+        Arrival in station with hour fixed
+    """
     def test_stop_point_2_city_arr_before(self):
         self.journey(_from="stop_point:CA1:SP:blr9",
                      to="admin:41047",
@@ -120,11 +137,13 @@ class TestTad(ArtemisTestFixture):
                      to="admin:41047",
                      datetime="20150319T102000")
 
+    # to is a coord in admin : admin:41047
     def test_stop_point_2_coord_arr_before(self):
         self.journey(_from="stop_point:CA1:SP:blr9",
                      to="1.3561;47.6089",
                      datetime="20150319T102000")
 
+    # to is a coord in admin : admin:41047
     def test_stop_area_2_coord_arr_before(self):
         self.journey(_from="stop_area:CA1:SA:blr9",
                      to="1.3561;47.6089",
@@ -140,6 +159,7 @@ class TestTad(ArtemisTestFixture):
                      to="stop_area:CA1:SA:blr9",
                      datetime="20150319T102000")
 
+    # _from is a coord in admin : admin:41047
     def test_coord_2_stop_point_arr_before(self):
         self.journey(_from="1.3561;47.6089",
                      to="stop_point:CA1:SP:blr9",
@@ -150,7 +170,11 @@ class TestTad(ArtemisTestFixture):
                      to="stop_area:CA1:SA:blr9",
                      datetime="20150319T102000")
 
-    # Tad commune a arret (arrivee avant & depart apres)
+    """
+    On demand transport tests : City to Station
+        Arrival in station with hour fixed
+        Departure from station with hour fixed
+    """
     def test_stop_point_2_city_arr_dep(self):
         self.journey(_from="stop_point:CA3:SP:blr9",
                      to="admin:41295",
@@ -161,11 +185,13 @@ class TestTad(ArtemisTestFixture):
                      to="admin:41295",
                      datetime="20150407T102000")
 
+    # to is a coord in admin : admin:41295
     def test_stop_point_2_coord_arr_dep(self):
         self.journey(_from="stop_point:CA3:SP:blr9",
                      to="1.3828;47.5831",
                      datetime="20150407T102000")
 
+    # to is a coord in admin : admin:41295
     def test_stop_area_2_coord_arr_dep(self):
         self.journey(_from="stop_area:CA3:SA:blr9",
                      to="1.3828;47.5831",
@@ -181,11 +207,13 @@ class TestTad(ArtemisTestFixture):
                      to="stop_area:CA3:SA:blr9",
                      datetime="20150407T102000")
 
+    # _from is a coord in admin : admin:41295
     def test_coord_2_stop_point_arr_dep(self):
         self.journey(_from="1.3828;47.5831",
                      to="stop_point:CA3:SP:blr9",
                      datetime="20150407T102000")
 
+    # _from is a coord in admin : admin:41295
     def test_coord_2_stop_area_arr_dep(self):
         self.journey(_from="1.3828;47.5831",
                      to="stop_area:CA3:SA:blr9",
