606d88dc680bba596aa6e873f71503949c125130
Pierre-Etienne Bougue <pierre-etienne.bougue@canaltp.fr>
2015-12-16 17:37:13 +0100

    update following navitia_image_manager/pull/11

--- artemis/default_settings_docker.py
+++ artemis/default_settings_docker.py
@@ -6,4 +6,4 @@ NAV_FILE_PATH_LAYOUT = "/srv/ed/data/{dataset}/data.nav.lz4"
 
 API_POINT_PREFIX = 'navitia/'
 
-JORMUNGANDR_DB = 'dbname=jormungandr user=jormungandr host=postgis password=jormungandr'
+JORMUNGANDR_DB = 'dbname=jormungandr user=jormungandr host=artemis_db password=jormungandr'
