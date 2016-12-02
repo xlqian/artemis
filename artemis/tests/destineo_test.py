a81571ab089304a225cf6f9870906bf2e886ea75
Noémie Lehuby <noemie.lehuby@openmailbox.org>
2016-12-02 14:21:56 +0100

    destineo - encoding bugfix

--- artemis/tests/destineo_test.py
+++ artemis/tests/destineo_test.py
@@ -147,7 +147,7 @@ class Destineo(object):
         We want to find the street "rue de la Loire (Saint-Sébastien-sur-Loire)" as the first answer.
 
         """
-        self.api('places?q=rue%20de%20la%20loire%20saint%20s%C3%A9bastien&count=1&')
+        self.api('places?q=rue%20de%20la%20loire%20saint%20sebastien&count=1&')
 
 @set_scenario({"fr-pdl": {"scenario": "destineo"}})
 class TestDestineo(Destineo, ArtemisTestFixture):
