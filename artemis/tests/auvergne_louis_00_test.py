8996f04cca785fb0787f0779693281d87686f264
Louis <test@test.com>
2018-07-23 16:51:04 +0200

    variables en dur -> default_settings.py ; ajout de 2 tests avec 1 appel de journey dans chaque

--- artemis/tests/auvergne_louis_00_test.py
+++ artemis/tests/auvergne_louis_00_test.py
@@ -12,9 +12,33 @@ class TestAuvergne(base_pytest.TestFixture):
         """
 
         # We shall just call the journey function here, the whole test is hidden inside.
-        base_pytest.journey_Test(self, _from="poi:osm:node:303386067",
+        self.journey(_from="poi:osm:node:303386067",
                            to="3.0630843999999797;45.7589254", datetime="20160121T170000",
                            first_section_mode=['bike', 'bss', 'walking', 'car'],
                            last_section_mode=['walking'],
                            min_nb_journeys=3,
                            max_duration_to_pt=1200)
+
+    def test_auvergne_02(self):
+        """
+        http://jira.canaltp.fr/browse/NAVITIAII-2016
+        """
+        self.journey(_from="admin:fr:63135",
+                     to="3.121833801269531;45.885276435738504", datetime="20160118T120300",
+                     first_section_mode=['bike', 'bss', 'walking', 'car'],
+                     last_section_mode=['walking'],
+                     min_nb_journeys=3,
+                     _night_bus_filter_base_factor=7200,
+                     max_duration_to_pt=1800)
+
+    def test_auvergne_03(self):
+        """
+        same that 02, but this time the nigth bus filter should remove walking solution since they are too late
+        """
+        self.journey(_from="admin:fr:63135",
+                     to="3.121833801269531;45.885276435738504", datetime="20160118T120300",
+                     first_section_mode=['bike', 'bss', 'walking', 'car'],
+                     last_section_mode=['walking'],
+                     min_nb_journeys=3,
+                     _night_bus_filter_base_factor=3600,
+                     max_duration_to_pt=1800)
--- artemis/tests/base_pytest.py
+++ artemis/tests/base_pytest.py
@@ -5,12 +5,7 @@ from flask import json
 import requests
 import inspect
 import re
-
-# Path to my artemis references
-PATH_REF = '/home/louis_gaillet/Projets/Artemis/artemis_references/'
-
-# Beginning of the URL : we want the request to go to my own Jormun on my own machine
-URL = 'http://127.0.0.1:9191/v1/coverage/default/journeys?'
+from artemis.configuration_manager import config
 
 def get_calling_test_function():
     """
@@ -39,6 +34,46 @@ class TestFixture(object):
         print file_name
         return file_name
 
+    def journey(self, _from, to, datetime,
+                datetime_represents='departure',
+                auto_from=None, auto_to=None,
+                first_section_mode=[], last_section_mode=[],
+                **kwargs):
+        """
+        This function is comming from the test_mechanism.py file.
+        We only use the part that generates the url.
+        Other parts are calling test that fail because we do not have the whole navitia running.
+        Thus, we do not need the "self" parameter, and response_checker is set to None.
+        We have also added parts of other functions into it.
+        Therefore, we only need to call journey and all the test are done from inside.
+        """
+
+        # Creating the URL with all the parameters for the query
+        assert datetime
+        query = "from={real_from}&to={real_to}&datetime={date}&datetime_represents={represent}". \
+            format(date=datetime, represent=datetime_represents,
+                   real_from=_from, real_to=to)
+        for mode in first_section_mode:
+            query = '{query}&first_section_mode[]={mode}'.format(query=query, mode=mode)
+
+        for mode in last_section_mode:
+            query = '{query}&last_section_mode[]={mode}'.format(query=query, mode=mode)
+
+        for k, v in kwargs.iteritems():
+            query = "{query}&{k}={v}".format(query=query, k=k, v=v)
+
+        # full URL concatenation
+        query = config['URL_JORMUN'] + query
+
+        # Get the json answer of the request (it is just a string here)
+        raw_response = requests.get(query)
+
+        # Transform the string into a dictionary
+        dict_resp = json.loads(raw_response.text)
+
+        # Comparing my response and my reference
+        compare_with_ref_louis(self, dict_resp)
+
 def compare_with_ref_louis(self, response,
                         response_checker=default_checker.default_journey_checker):
     """
@@ -56,7 +91,7 @@ def compare_with_ref_louis(self, response,
 
     # Add path to artemis references
     # config = flask_conf.Config(os.path.dirname(os.path.realpath(__file__)))
-    filepath = os.path.join(PATH_REF, filename)
+    filepath = os.path.join(config['PATH_REF'], filename)
 
     assert os.path.isfile(filepath)
     with open(filepath, 'r') as f:
@@ -89,43 +124,3 @@ def compare_with_ref_louis(self, response,
     """
     ### Compare answer and reference
     response_checker.compare(filtered_response, filtered_reference)
-
-def journey_Test(self, _from, to, datetime,
-            datetime_represents='departure',
-            auto_from=None, auto_to=None,
-            first_section_mode=[], last_section_mode=[],
-            **kwargs):
-    """
-    This function is comming from the test_mechanism.py file.
-    We only use the part that generates the url.
-    Other parts are calling test that fail because we do not have the whole navitia running.
-    Thus, we do not need the "self" parameter, and response_checker is set to None.
-    We have also added parts of other functions into it.
-    Therefore, we only need to call journey and all the test are done from inside.
-    """
-
-    # Creating the URL with all the parameters for the query
-    assert datetime
-    query = "from={real_from}&to={real_to}&datetime={date}&datetime_represents={represent}". \
-        format(date=datetime, represent=datetime_represents,
-               real_from=_from, real_to=to)
-    for mode in first_section_mode:
-        query = '{query}&first_section_mode[]={mode}'.format(query=query, mode=mode)
-
-    for mode in last_section_mode:
-        query = '{query}&last_section_mode[]={mode}'.format(query=query, mode=mode)
-
-    for k, v in kwargs.iteritems():
-        query = "{query}&{k}={v}".format(query=query, k=k, v=v)
-
-    # full URL concatenation
-    query = URL + query
-
-    # Get the json answer of the request (it is just a string here)
-    raw_response = requests.get(query)
-
-    # Transform the string into a dictionary
-    dict_resp = json.loads(raw_response.text)
-
-    # Comparing my response and my reference
-    compare_with_ref_louis(self, dict_resp)
